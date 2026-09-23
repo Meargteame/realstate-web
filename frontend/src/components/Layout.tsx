276eb5633b3292b7e752e3afb0d271094723e4ae
Meargteame <mearegteame99995555@gmail.com>
2026-09-23 09:24:56 +0300

    fix: replace AntLayout with plain div so sticky navbar works (AntLayout sets overflow:hidden internally)

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Layout as AntLayout } from "antd";
 import Header from "./Header";
 import Footer from "./Footer";
 import MobileNav from "./MobileNav";
 import { useIsMobile } from "../hooks/useBreakpoint";
 import { Outlet } from "react-router-dom";
 
-const { Content } = AntLayout;
-
 export default function Layout() {
   const isMobile = useIsMobile();
 
   return (
-    <AntLayout style={{ minHeight: '100vh', background: 'white' }}>
+    <div style={{ minHeight: "100vh", background: "#fff", display: "flex", flexDirection: "column" }}>
       <Header />
-      <Content style={{ display: 'flex', flexDirection: 'column', paddingBottom: isMobile ? 64 : 0 }}>
+      <div style={{ flex: 1, display: "flex", flexDirection: "column", paddingBottom: isMobile ? 64 : 0 }}>
         <Outlet />
-      </Content>
+      </div>
       <Footer />
       <MobileNav />
-    </AntLayout>
+    </div>
   );
 }
